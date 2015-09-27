a5bdd4c242d3022c8bb07592f4f2c69f1576b39f
Дамир Имангулов <damir.imangulov@dtech.company>
2015-09-27 16:24:21 +0300

    Задание с разделением чисел по весам готово, версия без рекурсии отрабатывает медленнее но работает более чем с 1000 чисел

--- generate_data_for_setup_balance.py
+++ generate_data_for_setup_balance.py
@@ -5,10 +5,8 @@ TEST_FILE = "test.txt"
 if os.path.isfile(TEST_FILE):
     os.remove(TEST_FILE)
 
-f = open("test.txt", "a")
-
 for n in range(2, 1002):
+    f = open("test.txt", "a")
     f.write(" %d" % n)
-
-f.close()
-os.system("python setup-balance.py < test.txt");
+    f.close()
+    os.system("python setup-balance.py < test.txt");
--- setup-balance.py
+++ setup-balance.py
@@ -1,19 +1,50 @@
 import sys
 
-EXCEED_SUM = "EXCEED_SUM"
-INDEX_ERROR = "INDEX_ERROR"
-GOOD = "GOOD"
-CANNOT_FIND_RIGHT = "CANNOT_FIND_RIGHT"
-OTHER_ERROR = "OTHER_ERROR"
 
 def main():
     data = sys.stdin.readlines()
     for line in data:
         numbers = [int(n) for n in line.split()]
-    numbers.sort()
-    findBalance(numbers)
 
-def findBalance(numbers):
+    if len(numbers) < 4:
+        print("need 4 or more numbers")
+        return
+
+    numbers.sort()
+    find_balance(numbers)
+
+    if find100(numbers, 0, 0):
+        print("yes")
+    else:
+        print("no")
+
+
+TARGET_SUM = 100
+
+
+def find100(numbers, index, currentSum):
+    if index >= len(numbers):
+        return False
+
+    n = numbers[index]
+
+    if n + currentSum > TARGET_SUM:
+        return False
+    else:
+        currentSum += n
+
+    if currentSum == TARGET_SUM:
+        return True
+
+    for i in range(index + 1, len(numbers)):
+        if find100(numbers, i, currentSum):
+            return True
+        continue
+
+    return False
+
+
+def find_balance(numbers):
     total = sum(numbers)
 
     if total % 2 != 0:
@@ -23,9 +54,8 @@ def findBalance(numbers):
     half = total / 2;
     left = []
     right = []
-    status = findNextForPart(numbers, 0, left, right, True, 0, half)
 
-    if status == GOOD:
+    if find_solution_for_part(numbers, left, right, True, half):
         leftStr = ""
         for n in left:
             leftStr += "%d " % n
@@ -36,50 +66,91 @@ def findBalance(numbers):
 
         print(leftStr + "- " + rightStr)
     else:
-        print("%s - balance not found", status)
-
-def findNextForPart(numbers, currentIndex, left, right, isLeft, currentSum, targetSum):
-    part = None
-    if isLeft:
-        part = left
-    else:
-        part = right
-
-    if currentIndex >= len(numbers):
-        return INDEX_ERROR
-
-    n = numbers[currentIndex]
-
-    if n + currentSum > targetSum:
-        return EXCEED_SUM
-    else:
-        part.append(n)
-        currentSum += n
-
-    if currentSum == targetSum:
-        if not isLeft:
-            return GOOD
-
-        rightNumbers = [x for x in numbers if x not in left]
-        if findNextForPart(rightNumbers, 0, left, right, False, 0, targetSum) == GOOD:
-            return GOOD
-        else:
-            part.remove(n)
-            currentSum -= n
-            return CANNOT_FIND_RIGHT
+        print("balance not found")
 
 
-    for i in range(currentIndex + 1, len(numbers)):
-        status = findNextForPart(numbers, i, left, right, isLeft, currentSum, targetSum)
+def find_solution_for_part(numbers, left, right, is_left, target_sum):
+    if numbers < 2:
+        return False
 
-        if status == GOOD:
-            return GOOD
+    bad_edges = {}
+    part = [0]
+    current_sum = numbers[0];
+    number_count = len(numbers)
 
-        if status == EXCEED_SUM:
+    found = False
+
+    while True:
+        if len(part) == 0:
+            return False
+
+        prev_node = part[len(part) - 1]
+        prev_n = numbers[prev_node]
+
+        curr_node = None
+        curr_n = None
+
+        path = str(part)
+        for x in range(prev_node + 1, number_count):
+            if path in bad_edges and numbers[x] in bad_edges[path]:
+                continue
+            curr_node = x
+            curr_n = numbers[curr_node]
             break
 
-    part.remove(n)
-    currentSum -= n
-    return OTHER_ERROR
+        if curr_node is None:
+            part.pop()
+            current_sum -= prev_n
+
+            path = str(part)
+            if path not in bad_edges:
+                bad_edges[path] = []
+            bad_edges[path].append(prev_n)
+            continue
+
+        if current_sum + curr_n > target_sum:
+            part.pop()
+            current_sum -= prev_n
+
+            path = str(part)
+            if path not in bad_edges:
+                bad_edges[path] = []
+            bad_edges[path].append(prev_n)
+            continue
+
+        current_sum += curr_n
+        part.append(curr_node)
+
+        if current_sum == target_sum:
+            if not is_left:
+                found = True
+                break
+
+            left_numbers = [numbers[index] for index in part]
+            right_numbers = [x for x in numbers if x not in left_numbers]
+
+            if find_solution_for_part(right_numbers, left, right, False, target_sum):
+                found = True
+                break
+
+            part.remove(curr_node)
+            current_sum -= curr_n
+            continue
+
+    if not found:
+        return False
+
+    target_part = None
+
+    if is_left:
+        target_part = left
+    else:
+        target_part = right
+
+    for i in part:
+        target_part.append(numbers[i])
+
+    return True
+
 
 main()
